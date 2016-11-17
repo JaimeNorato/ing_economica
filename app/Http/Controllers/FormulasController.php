7096035429005b89049576dd80ceed67092a0940
Jaime Norato <jn901113jn@gmail.com>
2016-11-17 16:36:18 -0500

    ejemplo, funcion para calcular i apartir de j y n

--- app/Http/Controllers/FormulasController.php
+++ app/Http/Controllers/FormulasController.php
@@ -6,5 +6,14 @@ use Illuminate\Http\Request;
 
 class FormulasController extends Controller
 {
-    //
+    /**
+     * calcula el interes efectivo (i) apartir del interes nominal (j)
+     * @param $j- interes nominal(en años)
+     * @param $n- periodo de tiempo en el que se aplica el tinteres (mensual=>12,trimestral=>4,semestral=>2, etc..)
+     * @return float
+     */
+    public function interesEfectivo($j,$n){
+        $i=$j/$n;/** interes efectivo(en meses) */
+        return $i;
+    }
 }
